19940ec3042620bbfd7f044663fca354091767fe
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2026-02-26 14:01:08 +0100

    Give up the dream of hecke.example()

--- src/Misc/interval.jl
+++ src/Misc/interval.jl
@@ -1,11 +1,11 @@
-# using Hecke
+using Hecke
 
 """
 Aaruni's special intervals for packing rectangles in squares
 """
 struct interval
-  a::Rational
-  b::Rational
+  a::QQFieldElem
+  b::QQFieldElem
   interval(a, b) = a > b ? error("out of order") : new(a, b)
 end
 
@@ -14,10 +14,10 @@ struct rectangle
   i2::interval    # interval on Y axis
 
   function rectangle(
-    a::Rational,
-    b::Rational,
-    c::Rational,
-    d::Rational
+    a::Union{Rational, QQFieldElem},
+    b::Union{Rational, QQFieldElem},
+    c::Union{Rational, QQFieldElem},
+    d::Union{Rational, QQFieldElem}
   )
     @assert a<b "out of order"
     @assert c<d "out of order"
@@ -26,8 +26,8 @@ struct rectangle
 
   function rectangle(
     k::Integer,
-    x::Rational=0//1,
-    y::Rational=0//1,
+    x::QQFieldElem=QQ(0),
+    y::QQFieldElem=QQ(0),
     r::Bool=false
   )
     i1 = interval(0, 1//(k+1))
@@ -40,36 +40,36 @@ struct rectangle
     return new(i1, i2)
   end
 
-#  function rectangle(k::Integer, x::Rational=0, y::Rational=0, r::Bool=false)
-#    println(k)
-#    println(x)
-#    println(y)
-#    println(r)
-#    return new(k, x//1, y//1, r)
-#  end
+  function rectangle(k::Integer, x::Rational=0, y::Rational=0, r::Bool=false)
+    println(k)
+    println(x)
+    println(y)
+    println(r)
+    return rectangle(k, QQ(x), QQ(y), r)
+  end
 
   function rectangle(k::Integer, r::Bool=false)
-    return rectangle(k, 0//1, 0//1, r)
+    return rectangle(k, QQ(0), QQ(0), r)
   end
 
   rectangle(i1::interval, i2::interval) = new(i1, i2)
 end
 
-function Base.:+(i::interval, x::Rational)
+function Base.:+(i::interval, x::QQFieldElem)
   return interval(i.a+x, i.b+x)
 end
 
-#function Base.:+(i::interval, x::Rational)
-#  return i+x
-#end
-
-function Base.:+(x::Rational, i::interval)
-  return i + x
+function Base.:+(i::interval, x::Rational)
+  return i+QQ(x)
 end
 
-#function Base.:+(x::Rational, i::interval)
-#  return i + x
-#end
+function Base.:+(x::Rational, i::interval)
+  return i + QQ(x)
+end
+
+function Base.:+(x::QQFieldElem, i::interval)
+  return i + x
+end
 
 function midpoint(i::interval)
   return (i.a + i.b ) // 2
@@ -89,34 +89,31 @@ end
 """
 test test test
 """
-function intersect(a::interval, b::interval)
+function intersect(a::interval, b::interval) :: Union{interval, Bool}
   try
     i = interval(maximum([a.a, b.a]), minimum([a.b, b.b]))
-    return (true, i)
+    return i
   catch
-    return (false, nothing)
+    return false
   end
 end
 
-function intersect(r1::rectangle, r2::rectangle)
+function intersect(r1::rectangle, r2::rectangle) :: Union{rectangle, Bool}
   i1 = intersect(r1.i1, r2.i1)
   i2 = intersect(r1.i2, r2.i2)
-  print(i1)
-  print(i2)
-  if !i1[1] || !i2[1]
-    return (false, nothing)
+  if typeof(i1) == Bool || typeof(i2) == Bool
+    return false
   end
-  return (true, rectangle(i1[2], i2[2]))
+  return rectangle(i1, i2)
 end
+#export interval
 
-function rectarray()
-  return [
-    rectangle(1),
-    rectangle(2, 1//2, 0//1, true),
-    rectangle(3),
-    rectangle(4)
-  ]
-end
+rectarray = [
+  rectangle(1),
+  rectangle(2, QQ(1//2), QQ(0), true),
+  rectangle(3),
+  rectangle(4)
+]
 
 #=
 if :Plots in names(Main, imported=true)
